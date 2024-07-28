394a40a2857becb8b048f03c6989acbd72865b66
Garrick Aden-Buie <garrick@adenbuie.com>
2024-07-28 14:50:44 -0400

    first example

---
+++ exercises/01_app.R
@@ -0,0 +1,53 @@
+# ┌ level-up-shiny ──────────────────────────────────┐
+# │                                                 │
+# │                   Exercise 1                    │
+# │                                                 │
+# └─────────────────────────────── posit::conf(2024) ┘
+#
+# TASKS:
+# 1. Run the app and use it to learn about the `school` and `scorecard` datasets.
+#
+# 2. Load {bslib} and change the theme of the app, using your favorite colors
+#    for the background and foreground colors.
+#    *Hint:* Check out https://coolors.co for color inspiration.
+#
+# 3. Choose an accent color for the primary color of the app.
+#
+# 4. Add `thematic::thematic_shiny()` to the app to make the plots look better.
+
+library(shiny)
+
+ui <- fluidPage(
+  selectizeInput("data", "Data set", choices = c("school", "scorecard"), selected = "school"),
+  radioButtons("type", "Inspection type", choices = c("Column Types" = "types", "Categorical" = "cat", "Numeric" = "num", "Missing" = "na"), inline = TRUE),
+  plotOutput("plot")
+)
+
+server <- function(input, output, session) {
+  data <- reactive({
+    switch(
+      input$data,
+      "school" = collegeScorecard::school,
+      "scorecard" = collegeScorecard::scorecard
+    )
+  })
+
+  output$plot <- renderPlot({
+    req(data())
+
+    df <- data()
+
+    inspected <- switch(
+      input$type,
+      "types" = inspectdf::inspect_types(df),
+      "cat" = inspectdf::inspect_cat(df),
+      "num" = inspectdf::inspect_num(df),
+      "na" = inspectdf::inspect_na(df)
+    )
+
+    inspectdf::show_plot(inspected, col_palette = 2)
+  })
+}
+
+shinyApp(ui, server)
+
---
+++ exercises/01_solution_app.R
@@ -0,0 +1,45 @@
+library(shiny)
+library(bslib)
+
+thematic::thematic_shiny()
+
+ui <- fluidPage(
+  theme = bs_theme(
+    version = 5,
+    bg = "#0B3954",
+    fg = "#bfd7ea",
+    primary = "#0BAFC1",
+  ),
+  selectizeInput("data", "Data set", choices = c("school", "scorecard"), selected = "school"),
+  radioButtons("type", "Inspection type", choices = c("Column Types" = "types", "Categorical" = "cat", "Numeric" = "num", "Missing" = "na"), inline = TRUE),
+  plotOutput("plot")
+)
+
+server <- function(input, output, session) {
+  data <- reactive({
+    switch(
+      input$data,
+      "school" = collegeScorecard::school,
+      "scorecard" = collegeScorecard::scorecard
+    )
+  })
+
+  output$plot <- renderPlot({
+    req(data())
+
+    df <- data()
+
+    inspected <- switch(
+      input$type,
+      "types" = inspectdf::inspect_types(df),
+      "cat" = inspectdf::inspect_cat(df),
+      "num" = inspectdf::inspect_num(df),
+      "na" = inspectdf::inspect_na(df)
+    )
+
+    inspectdf::show_plot(inspected, col_palette = 2)
+  })
+}
+
+shinyApp(ui, server)
+
